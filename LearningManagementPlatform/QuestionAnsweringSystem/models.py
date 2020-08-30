6580981be8cce305379bf8e9e488c64a06c3bd79
epsilon938 <epsilone938@gmail.com>
2020-08-30 12:34:06 +0530

    Added Register Functionality with basic functional based views

--- LearningManagementPlatform/QuestionAnsweringSystem/models.py
+++ LearningManagementPlatform/QuestionAnsweringSystem/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    username = models.CharField(max_length=200)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=100)
+    college = models.CharField(max_length=100)
+    roll_number = models.BigIntegerField(primary_key=True)
+    phone_number = models.BigIntegerField()
+    state = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
--- LearningManagementPlatform/QuestionAnsweringSystem/views.py
+++ LearningManagementPlatform/QuestionAnsweringSystem/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, Http404
+from .models import Student
+from django.shortcuts import render
 # Create your views here.
+
+def index(request):
+
+    ''' this function returns home page
+
+    :returns HttpResponse: HomePage
+    '''
+
+    return render(request,"register.html")
+
+def register(request):
+
+    ''' this function takes user details as input and stores those student details in database
+
+    :param request: Student Details like Username, Password , Email etc.
+
+    :returns HttpResponse: Successful response if details are created successfully
+
+    '''
+    if request.method == "POST":
+        if request.POST:
+            data = request.POST
+            try:
+                student_obj = Student(
+                    username = data['username'],
+                    email = data['email'],
+                    password = data['password'],
+                    college = data['college'],
+                    roll_number = data['roll_number'],
+                    phone_number = data['phone_number'],
+                    state = data['state'],
+                    city = data['city'],
+                    country = data['country']
+                )
+                student_obj.save()
+                return HttpResponse("You have successfully Registered")
+            except:
+                raise Http404("Validation Error")
+        else:
+            raise Http404("Page not found.")
+    else:
+        raise Http404("{} not available".format(request.method))
+
